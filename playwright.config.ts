319da4fbe5a1599025418cae5d4b144c7290fd12
igespinob <ignacio.espinosa-bonmati@cagemini.com>
2024-02-28 11:26:07 +0100

    Added support for sharding

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['allure-playwright']]
+  reporter: [['html'], ['allure-playwright'], ['blob']]
   ,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
